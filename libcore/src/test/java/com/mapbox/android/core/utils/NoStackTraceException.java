83730af77b584338ee1536927413c863f5fa63e8
Powerhouse Song <166808890+phcamai@users.noreply.github.com>
2024-06-04 22:58:02 +0800

    compile mapbox-gl-native success

--- libcore/src/test/java/com/mapbox/android/core/utils/NoStackTraceException.java
+++
@@ -1,8 +0,0 @@
-package com.mapbox.android.core.utils;
-
-public class NoStackTraceException extends Exception {
-  @Override
-  public synchronized Throwable fillInStackTrace() {
-    return null;
-  }
-}
